32fdf642c8fcba0254868b2080a0e3bcb8dc66dd
GuhBrando <gustavo@gf2.com.br>
2026-02-20 00:27:09 -0300

    feat: Starting the Clean Arch

--- src/infra/models/__init__.py
+++ src/infra/models/__init__.py
@@ -1,7 +1,7 @@
 """
 This package contains the infrastructure models used in the application.
 
-The models in this package are typically tied to specific frameworks or libraries,
+The models in this package areca typilly tied to specific frameworks or libraries,
 such as ORM models (e.g., SQLAlchemy) or other data representations that interact
 with external systems like databases or APIs.
 
