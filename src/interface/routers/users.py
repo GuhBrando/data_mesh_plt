c118de52e4920a6054ff4ef4e221133366821ca2
GuhBrando <gustavo@gf2.com.br>
2026-02-20 00:28:17 -0300

    fix: User router

--- src/interface/routers/users.py
+++ src/interface/routers/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 
-from src.use_cases import create_user_interactor
+from src.domain import user_persistence
 
 router = APIRouter()
 
@@ -8,7 +8,7 @@ router = APIRouter()
 @router.post("/users", response_model=UserResponseModel)
 async def create_user(user: UserCreateModel):
     try:
-        created_user = await create_user_interactor(user)
+        created_user = await user_persistence(user)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     else:
