44ca831561c404e06b9a08202dee75b21c1e93f7
GuhBrando <gustavo@gf2.com.br>
2026-02-24 13:01:00 -0300

    feat: Adjustments in interface and user schema

--- src/interface/routers/users.py
+++ src/interface/routers/users.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 
-from src.domain import user_persistence
+
+from src.interface.schemas.user import UserCreateModel, UserResponseModel
+from src.use_cases.user_interactor import CreateUserInteractor
 
 router = APIRouter()
 
@@ -8,7 +11,7 @@ router = APIRouter()
 @router.post("/users", response_model=UserResponseModel)
 async def create_user(user: UserCreateModel):
     try:
-        created_user = await user_persistence(user)
+        created_user = await CreateUserInteractor(user).execute()
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     else:
--- src/interface/schemas/user.py
+++ src/interface/schemas/user.py
@@ -1,7 +1,42 @@
 from pydantic import BaseModel
 
+class UserCreateModel(BaseModel):
+    """
+    Represents the data required to create a new user.
+
+    Attributes:
+        username (str): The username of the user.
+        email (str): The email address of the user.
+        password (str): The password for the user account.
+    """
+    username: str
+    email: str
+    password: str
+
+
+class UserResponseModel(BaseModel):
+    """
+    Represents the data returned as a response for user-related operations.
+
+    Attributes:
+        id (int): The unique identifier of the user.
+        username (str): The username of the user.
+        email (str): The email address of the user.
+    """
+    id: int
+    username: str
+    email: str
+
 
 class UserIdentity(BaseModel):
+    """
+    Represents the identity details of a user.
+
+    Attributes:
+        id (int): The unique identifier of the user.
+        name (str): The name of the user.
+        email (str): The email address of the user.
+    """
     id: int
     name: str
     email: str
