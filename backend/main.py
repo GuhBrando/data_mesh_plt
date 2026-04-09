48392cde6cdc2cb2aea0fd3ca5c29bee2a438773
GuhBrando <gustavo@gf2.com.br>
2026-04-09 17:31:45 -0300

    fix: Fixed CORS ind cd and added secret in github

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from backend.infra.config import CORS_ORIGINS
 from backend.interface.routers import api_router
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=CORS_ORIGINS,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
