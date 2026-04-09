611ca51d909fa24d43b3b09008d59e300d07431f
Gustavo Brandao <gustavo@gf2.com.br>
2026-04-09 20:51:34 -0300

    fix: Changed to no SSL

--- backend/infra/postgres.py
+++ backend/infra/postgres.py
@@ -14,7 +14,7 @@ async def get_db_connection():
         database=DB_NAME,
         host=DB_HOST,
         port=DB_PORT,
-        ssl="require",
+        ssl="disable",
     )
     try:
         yield conn
