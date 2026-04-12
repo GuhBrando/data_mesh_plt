c1574d4a84ce0cb7e2d6f12582c08362d91528cc
Gustavo Brandao <gustavo@gf2.com.br>
2026-04-12 20:24:49 -0300

    fix: Changed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 atlas migrate apply \
   --env azure \
-  --config /app/database/atlas.hcl
+  --config file:///app/database/atlas.hcl
 
 exec uv run uvicorn backend.main:app --host 0.0.0.0 --port 8000
