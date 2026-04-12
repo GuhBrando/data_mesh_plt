256ba5da737244eaf5980df8bb4bcdd03567ec07
Gustavo Brandao <gustavo@gf2.com.br>
2026-04-12 20:35:55 -0300

    fix: Changed the main folder for entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-atlas migrate apply \
-  --env azure \
-  --config file:///app/database/atlas.hcl
+cd /app/database
+atlas migrate apply --env azure
 
+cd /app
 exec uv run uvicorn backend.main:app --host 0.0.0.0 --port 8000
